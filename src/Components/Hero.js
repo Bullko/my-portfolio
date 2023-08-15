ee44b42da87ffa8171dcd14e6a8859c34ca83442
BullsHI7 <krajnak.anton@gmail.com>
2023-08-15 12:06:40 +0200

    Hero section added

---
+++ src/Components/Hero.js
@@ -0,0 +1,15 @@
+import HeroStyles from "./Hero.css";
+
+export default function Hero() {
+  return (
+    <div className="hero">
+      <div className="profile-pic">
+        <img />
+      </div>
+      <div>
+        <h1>Hello!</h1>
+        <h4>lorem 10</h4>
+      </div>
+    </div>
+  );
+}
